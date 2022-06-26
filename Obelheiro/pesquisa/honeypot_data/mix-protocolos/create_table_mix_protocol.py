9faf430c11cdc483aadd1275cf8a60c48970b94a
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-06-26 14:41:49 -0300

    add victim ip

--- Obelheiro/pesquisa/honeypot_data/mix-protocolos/create_table_mix_protocol.py
+++ Obelheiro/pesquisa/honeypot_data/mix-protocolos/create_table_mix_protocol.py
@@ -10,7 +10,8 @@ CREATE TABLE MIX_PROTOCOL (
   attack_protocol TEXT NOT NULL,
 	requests_per_attack INTEGER NOT NULL,
   tempo_inicio TEXT NOT NULL,
-  tempo_final TEXT NOT NULL
+  tempo_final TEXT NOT NULL,
+  vitima_ip TEXT NOT NULL
 );
 """)
 mix_connection.commit()
@@ -22,7 +23,7 @@ ntp_connection = sqlite3.connect('../db/database-2022-05-11/dnstor_statistics_nt
 ntp_cursor = ntp_connection.cursor()
 
 for ntp_row in ntp_cursor.execute("""
-SELECT count, tempoInicio, tempoFinal
+SELECT count, tempoInicio, tempoFinal, ip
   FROM NTP_MEMORY_DICT
   JOIN NTP_PAYLOAD_DICT
     ON NTP_MEMORY_DICT.payloadID = NTP_PAYLOAD_DICT.payloadID
@@ -31,14 +32,15 @@ SELECT count, tempoInicio, tempoFinal
   count = int(ntp_row[0])
   tempoInicio = ntp_row[1]
   tempoFinal = ntp_row[2]
+  ipVitima = ntp_row[3]
 
-  protocol_mix.append((protocol_id, "NTP", count, tempoInicio, tempoFinal))
+  protocol_mix.append((protocol_id, "NTP", count, tempoInicio, tempoFinal, ipVitima))
   protocol_id += 1
 
 ntp_connection.close()
 print('ntp finish: ', len(protocol_mix))
 
-mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?)', protocol_mix)
+mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?,?)', protocol_mix)
 mix_connection.commit()
 
 
@@ -47,19 +49,20 @@ dns_connection = sqlite3.connect('../db/database-2022-05-11/dnstor_statistics_dn
 dns_cursor = dns_connection.cursor()
 
 for dns_row in dns_cursor.execute("""
-  SELECT count, tempoInicio, tempoFinal
+  SELECT count, tempoInicio, tempoFinal, ip
     FROM DNS_MEMORY_DICT;
 """):
   count = int(dns_row[0])
   tempoInicio = dns_row[1]
   tempoFinal = dns_row[2]
+  ipVitima = dns_row[3]
 
-  protocol_mix.append((protocol_id, "DNS", count, tempoInicio, tempoFinal))
+  protocol_mix.append((protocol_id, "DNS", count, tempoInicio, tempoFinal, ipVitima))
   protocol_id += 1
 
 dns_connection.close()
 
-mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?)', protocol_mix)
+mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?,?)', protocol_mix)
 mix_connection.commit()
 print('dns finish: ', len(protocol_mix))
 
@@ -69,19 +72,20 @@ chargen_connection = sqlite3.connect('../db/database-2022-05-11/dnstor_statistic
 chargen_cursor = chargen_connection.cursor()
 
 for chargen_row in chargen_cursor.execute("""
-  SELECT count, tempoInicio, tempoFinal
+  SELECT count, tempoInicio, tempoFinal, ip
     FROM CHARGEN_MEMORY_DICT;
 """):
   count = int(chargen_row[0])
   tempoInicio = chargen_row[1]
   tempoFinal = chargen_row[2]
+  ipVitima = chargen_row[3]
 
-  protocol_mix.append((protocol_id, "CHARGEN", count, tempoInicio, tempoFinal))
+  protocol_mix.append((protocol_id, "CHARGEN", count, tempoInicio, tempoFinal, ipVitima))
   protocol_id += 1
 
 chargen_connection.close()
 
-mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?)', protocol_mix)
+mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?,?)', protocol_mix)
 mix_connection.commit()
 print('chargen finish: ', len(protocol_mix))
 
@@ -92,19 +96,20 @@ cldap_connection = sqlite3.connect('../db/database-2022-05-11/dnstor_statistics_
 cldap_cursor = cldap_connection.cursor()
 
 for cldap_row in cldap_cursor.execute("""
-  SELECT count, tempoInicio, tempoFinal
+  SELECT count, tempoInicio, tempoFinal, ip
     FROM CLDAP_MEMORY_DICT;
 """):
   count = int(cldap_row[0])
   tempoInicio = cldap_row[1]
   tempoFinal = cldap_row[2]
+  ipVitima = cldap_row[3]
 
-  protocol_mix.append((protocol_id, "CLDAP", count, tempoInicio, tempoFinal))
+  protocol_mix.append((protocol_id, "CLDAP", count, tempoInicio, tempoFinal, ipVitima))
   protocol_id += 1
 
 cldap_connection.close()
 
-mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?)', protocol_mix)
+mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?,?)', protocol_mix)
 mix_connection.commit()
 print('cldap finish: ', len(protocol_mix))
 
@@ -114,19 +119,20 @@ coap_connection = sqlite3.connect('../db/database-2022-05-11/dnstor_statistics_c
 coap_cursor = coap_connection.cursor()
 
 for coap_row in coap_cursor.execute("""
-  SELECT count, tempoInicio, tempoFinal
+  SELECT count, tempoInicio, tempoFinal, ip
     FROM COAP_MEMORY_DICT;
 """):
   count = int(coap_row[0])
   tempoInicio = coap_row[1]
   tempoFinal = coap_row[2]
+  ipVitima = coap_row[3]
 
-  protocol_mix.append((protocol_id, "COAP", count, tempoInicio, tempoFinal))
+  protocol_mix.append((protocol_id, "COAP", count, tempoInicio, tempoFinal, ipVitima))
   protocol_id += 1
 
 coap_connection.close()
 
-mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?)', protocol_mix)
+mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?,?)', protocol_mix)
 mix_connection.commit()
 print('coap finish: ', len(protocol_mix))
 
@@ -135,19 +141,20 @@ memcached_connection = sqlite3.connect('../db/database-2022-05-11/dnstor_statist
 memcached_cursor = memcached_connection.cursor()
 
 for memcached_row in memcached_cursor.execute("""
-  SELECT count, tempoInicio, tempoFinal
+  SELECT count, tempoInicio, tempoFinal, ip
     FROM MEMCACHED_MEMORY_DICT;
 """):
   count = int(memcached_row[0])
   tempoInicio = memcached_row[1]
   tempoFinal = memcached_row[2]
+  ipVitima = memcached_row[3]
 
-  protocol_mix.append((protocol_id, "MEMCACHED", count, tempoInicio, tempoFinal))
+  protocol_mix.append((protocol_id, "MEMCACHED", count, tempoInicio, tempoFinal, ipVitima))
   protocol_id += 1
 
 memcached_connection.close()
 
-mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?)', protocol_mix)
+mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?,?)', protocol_mix)
 mix_connection.commit()
 print('memcached finish: ', len(protocol_mix))
 
@@ -158,19 +165,20 @@ qotd_connection = sqlite3.connect('../db/database-2022-05-11/dnstor_statistics_q
 qotd_cursor = qotd_connection.cursor()
 
 for qotd_row in qotd_cursor.execute("""
-  SELECT count, tempoInicio, tempoFinal
+  SELECT count, tempoInicio, tempoFinal, ip
     FROM QOTD_MEMORY_DICT;
 """):
   count = int(qotd_row[0])
   tempoInicio = qotd_row[1]
   tempoFinal = qotd_row[2]
+  ipVitima = qotd_row[3]
 
-  protocol_mix.append((protocol_id, "QOTD", count, tempoInicio, tempoFinal))
+  protocol_mix.append((protocol_id, "QOTD", count, tempoInicio, tempoFinal, ipVitima))
   protocol_id += 1
 
 qotd_connection.close()
 
-mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?)', protocol_mix)
+mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?,?)', protocol_mix)
 mix_connection.commit()
 print('qotd finish: ', len(protocol_mix))
 
@@ -181,19 +189,20 @@ ssdp_connection = sqlite3.connect('../db/database-2022-05-11/dnstor_statistics_s
 ssdp_cursor = ssdp_connection.cursor()
 
 for ssdp_row in ssdp_cursor.execute("""
-  SELECT count, tempoInicio, tempoFinal
+  SELECT count, tempoInicio, tempoFinal, ip
     FROM SSDP_MEMORY_DICT;
 """):
   count = int(ssdp_row[0])
   tempoInicio = ssdp_row[1]
   tempoFinal = ssdp_row[2]
+  ipVitima = ssdp_row[3]
 
-  protocol_mix.append((protocol_id, "SSDP", count, tempoInicio, tempoFinal))
+  protocol_mix.append((protocol_id, "SSDP", count, tempoInicio, tempoFinal, ipVitima))
   protocol_id += 1
 
 ssdp_connection.close()
 
-mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?)', protocol_mix)
+mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?,?)', protocol_mix)
 mix_connection.commit()
 print('ssdp finish: ', len(protocol_mix))
 
@@ -203,19 +212,20 @@ steam_games_connection = sqlite3.connect('../db/database-2022-05-11/dnstor_stati
 steam_games_cursor = steam_games_connection.cursor()
 
 for steam_games_row in steam_games_cursor.execute("""
-  SELECT count, tempoInicio, tempoFinal
+  SELECT count, tempoInicio, tempoFinal, ip
     FROM MEMORY_DICT;
 """):
   count = int(steam_games_row[0])
   tempoInicio = steam_games_row[1]
   tempoFinal = steam_games_row[2]
+  ipVitima = steam_games_row[3]
 
-  protocol_mix.append((protocol_id, "STEAM_GAMES", count, tempoInicio, tempoFinal))
+  protocol_mix.append((protocol_id, "STEAM_GAMES", count, tempoInicio, tempoFinal, ipVitima))
   protocol_id += 1
 
 steam_games_connection.close()
 
-mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?)', protocol_mix)
+mix_cursor.executemany('INSERT INTO MIX_PROTOCOL VALUES (?,?,?,?,?,?)', protocol_mix)
 mix_connection.commit()
 print('steam_games finish: ', len(protocol_mix))
 
