af5b621b670a264c292064df8b773986a4b2cded
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2022-08-26 00:09:40 -0300

    get mut hashmap

--- Obelheiro/pesquisa/honeypot_data/rust/pcap_reader/src/main.rs
+++ Obelheiro/pesquisa/honeypot_data/rust/pcap_reader/src/main.rs
@@ -9,22 +9,6 @@ use chrono::{DateTime, FixedOffset};
 mod pcap_packet;
 // use std::path::Path;
 
-pub struct PcapAttack {
-  id: i32,
-  ip_vitima_cidr: Ipv4Cidr,
-  packets: Vec<pcap_packet::PcapPacket>,
-  timestamp_inicio: DateTime<FixedOffset>,
-  timestamp_fim: DateTime<FixedOffset>,
-}
-
-fn get_current_working_dir() -> PathBuf {
-  let working_dir = env::current_dir();
-  let current_dir = match working_dir {
-    Ok(workdir) => workdir,
-    Err(error) => panic!("Problem reading current dir: {:?}", error),
-  };
-  return current_dir;
-}
 
 fn main() -> Result<()> {
   let currently_dir = get_current_working_dir();
@@ -56,17 +40,44 @@ fn main() -> Result<()> {
     .spawn()
     .unwrap_or_else(|e| panic!("Error starting tshark: {e}"));
 
-  pcap_process(rtshark, &conn, &mut map_id, &mut map_attacks, &mut attack_id);
+  pcap_process(rtshark, &conn, &mut map_id, &mut map_attacks);
 
   Ok(())
 }
+
+pub struct PcapAttack {
+  ip_vitima_cidr: Ipv4Cidr,
+  packets: Vec<pcap_packet::PcapPacket>,
+  timestamp_inicio: DateTime<FixedOffset>,
+  timestamp_fim: DateTime<FixedOffset>,
+}
+
+impl PcapAttack {
+  pub fn add_packet(&mut self, packet: pcap_packet::PcapPacket) {
+    self.timestamp_fim = packet.frame.timestamp;
+    self.packets.push(packet);
+  }
+
+  // pub fn same_attack(&self, packet: pcap_packet::PcapPacket) -> bool {
+  //   self.timestamp_fim.
+  // }
+}
+
+fn get_current_working_dir() -> PathBuf {
+  let working_dir = env::current_dir();
+  let current_dir = match working_dir {
+    Ok(workdir) => workdir,
+    Err(error) => panic!("Problem reading current dir: {:?}", error),
+  };
+  return current_dir;
+}
+
 
 pub fn pcap_process(
   mut rtshark: RTShark,
   conn: &Connection,
   map_id: &mut HashMap<&str, i32>,
   map_attacks: &mut HashMap<Ipv4Cidr, HashMap<i32, PcapAttack>>,
-  attack_id: &mut i32,
 ) {
   // read packets until the end of the PCAP file
 
@@ -87,50 +98,51 @@ pub fn pcap_process(
       // }
 
     let packet = pcap_packet::pcap_process_packet(packet, conn, map_id);
-    add_packet_to_attacks(map_attacks, packet, *attack_id);
-
-    *attack_id += 1;
-  }
+    add_packet_to_attacks(map_attacks, packet);
+ }
 }
 
-pub fn add_packet_to_attacks(cidr_udp_attack: &mut HashMap<Ipv4Cidr, HashMap<i32, PcapAttack>>, packet: pcap_packet::PcapPacket, attack_id: i32) {
+pub fn add_packet_to_attacks(cidr_udp_attack: &mut HashMap<Ipv4Cidr, HashMap<i32, PcapAttack>>, packet: pcap_packet::PcapPacket) {
   let cidr = packet.ip.vitima_cidr;
 
   match cidr_udp_attack.get_mut(&cidr) {
     Some(udp_attack) => {
       // cidr existe
       let udp_dest_port = packet.udp.destination_port;
-      udp_attack.insert(udp_dest_port, new_attack(packet, attack_id));
+
+
+      match udp_attack.get_mut(&udp_dest_port) {
+        Some(attack) => {
+          // attack exists
+          // check time
+
+        },
+        None => {
+          // no attack
+          udp_attack.insert(udp_dest_port, new_empty_attack(packet));
+        },
+      }
     },
+
     None => {
       // não tem o cidr,
-      let udp_attack = new_map_attack(packet, attack_id);
+      let udp_attack = new_map_attack(packet);
       cidr_udp_attack.insert(cidr, udp_attack);
     },
   }
-
-
-  // if !udp_attack.contains_key(&udp_dest_port) {
-  //   // tem o cidr
-  //   // mas nao tem a porta de destino udp
-  //   // let attack = new_attack(packet, attack_id);
-  //   // udp_attack.insert(udp_dest_port, attack);
-  //   return ;
-  // }
 }
 
-pub fn new_map_attack(packet: pcap_packet::PcapPacket, attack_id: i32) -> HashMap<i32, PcapAttack> {
+pub fn new_map_attack(packet: pcap_packet::PcapPacket) -> HashMap<i32, PcapAttack> {
   let mut map_attack = HashMap::<i32, PcapAttack>::new();
-  map_attack.insert(packet.udp.destination_port, new_attack(packet, attack_id));
+  map_attack.insert(packet.udp.destination_port, new_empty_attack(packet));
 
   return map_attack;
 }
 
-pub fn new_attack(packet: pcap_packet::PcapPacket, attack_id: i32) -> PcapAttack {
+pub fn new_empty_attack(packet: pcap_packet::PcapPacket) -> PcapAttack {
   let timestamp = packet.frame.timestamp;
 
   return PcapAttack{
-    id: attack_id,
     ip_vitima_cidr: packet.ip.vitima_cidr,
     packets: vec![packet],
     timestamp_inicio: timestamp,
